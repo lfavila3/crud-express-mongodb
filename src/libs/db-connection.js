be2772d82d1d31c191fce1961640d5920d7807d2
lfavila3 <ferchoavila14@gmail.com>
2020-08-08 22:36:56 -0500

    Se ha creado el crud de tareas

---
+++ src/libs/db-connection.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+let db;
+
+module.exports = function Connection() {
+    if (!db) {
+        db = mongoose.createConnection('mongodb://localhost/crud-example', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+    }
+    return db;
+}
